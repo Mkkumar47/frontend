6a688e48b75524b4b8b01ee15ae0b9c827e37b58
Mkkumar47 <manikanta.redrouthu47@gmail.com>
2026-06-14 21:30:13 +0530

    api url added in env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://backend-gamma-eight-14.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 20000
 });
 
